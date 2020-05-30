b1f6a3399852c108f2f43bcb9be9ffad5feff5b4
Thales Marega <thales.bm92@gmail.com>
2020-05-30 13:22:43 -0300

    first commit

---
+++ app/src/main/java/bm/it/mobile/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package bm.it.mobile.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import bm.it.mobile.app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ library/src/main/java/bm/it/mobile/library/MainActivity.kt
@@ -0,0 +1,13 @@
+package bm.it.mobile.library
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import bm.it.mobile.library.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
